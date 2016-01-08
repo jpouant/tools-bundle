7d59b196260fd1dbbe9faacac220ce67bd173a74
Adrien Schaegis <adrien@iron-mail.net>
2016-01-08 14:34:15 +0000

    [UPDATE] Add the copy recursively directory

--- Utils/PathUtils.php
+++ Utils/PathUtils.php
@@ -13,4 +13,47 @@ class PathUtils
     {
         return preg_replace('#^(\$HOME|~)(/|$)#', rtrim(getenv('HOME') ?: getenv('USERPROFILE'), '/\\') . '/', $haystack);
     }
+
+    /**
+     * Copy directory recursively
+     *
+     * @param string $source
+     * @param string $dest
+     *
+     * @return bool
+     */
+    public static function copyr($source, $dest)
+    {
+        // Check for symlinks
+        if (is_link($source)) {
+            return symlink(readlink($source), $dest);
+        }
+
+        // Simple copy for a file
+        if (is_file($source)) {
+            return copy($source, $dest);
+        }
+
+        // Make destination directory
+        if (!is_dir($dest)) {
+            mkdir($dest);
+        }
+
+        // Loop through the folder
+        $dir = dir($source);
+        while (false !== $entry = $dir->read()) {
+            // Skip pointers
+            if ($entry == '.' || $entry == '..') {
+                continue;
+            }
+
+            // Deep copy directories
+            static::copyr("$source/$entry", "$dest/$entry");
+        }
+
+        // Clean up
+        $dir->close();
+
+        return true;
+    }
 }
