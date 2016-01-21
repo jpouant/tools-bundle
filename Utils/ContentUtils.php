59143d61d5d36dba65cafbb9b8b0222aabf31df6
Adrien Schaegis <adrien@iron-mail.net>
2016-01-21 12:23:50 +0000

    [UPDATE] Add strip whitespace to contets utils

--- Utils/ContentUtils.php
+++ Utils/ContentUtils.php
@@ -13,7 +13,7 @@ class ContentUtils
      */
     public static function arrayToString(array $array, $eol = '', $level = 0)
     {
-        $result = '';
+        $result       = '';
         $spacesIndent = $spacesStart = '';
         for ($i = 0; $i <= $level; $i++) {
             if ($i > 0) {
@@ -37,5 +37,40 @@ class ContentUtils
         $result .= $spacesStart;
 
         return $result;
+    }
+
+    /**
+     * Removes whitespace from a PHP source string while preserving line numbers.
+     *
+     * @param  string $source A PHP string
+     *
+     * @return string The PHP string with the whitespace removed
+     */
+    public static function stripWhitespace($source)
+    {
+        if (!function_exists('token_get_all')) {
+            return $source;
+        }
+
+        $output = '';
+        foreach (token_get_all($source) as $token) {
+            if (is_string($token)) {
+                $output .= $token;
+            } elseif (in_array($token[0], [T_COMMENT, T_DOC_COMMENT])) {
+                $output .= str_repeat("\n", substr_count($token[1], "\n"));
+            } elseif (T_WHITESPACE === $token[0]) {
+                // reduce wide spaces
+                $whitespace = preg_replace('{[ \t]+}', ' ', $token[1]);
+                // normalize newlines to \n
+                $whitespace = preg_replace('{(?:\r\n|\r|\n)}', "\n", $whitespace);
+                // trim leading spaces
+                $whitespace = preg_replace('{\n +}', "\n", $whitespace);
+                $output .= $whitespace;
+            } else {
+                $output .= $token[1];
+            }
+        }
+
+        return $output;
     }
 }
