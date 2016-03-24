c726046952d6396e3c2e43b63d4f40111c901876
Adrien Schaegis <adrien@iron-mail.net>
2016-03-24 16:27:05 +0000

    [UPDATE] Comments + depth retrievable

--- Utils/ObjectUtils.php
+++ Utils/ObjectUtils.php
@@ -5,17 +5,19 @@ namespace Neirda24\Bundle\ToolsBundle\Utils;
 class ObjectUtils
 {
     /**
-     * Removes whitespace from a PHP source string while preserving line numbers.
+     * Retrieve the caller of a function.
+     *
+     * @param int $depth To get the parent use 1. To get its parent use 2, ..., ...
      *
      * @return mixed
      */
-    public static function retrieveCaller()
+    public static function retrieveCaller($depth = 1)
     {
         // 1 Contains ContentUtils
         // 2 Contains The caller of this function
         // 3 Contains what you want
 
-        $debug = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 3);
+        $debug = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 2 + $depth);
         $backtrace = array_pop($debug);
         $caller = $backtrace['object'];
 
